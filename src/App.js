1547b4a3808b89ab07b8179a56b2045db96c26b1
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-01-11 15:39:35 -0500

    Practicas iniciales

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-/* import './App.css'; */
 import React from 'react';
-import Contador from './components/Contador';
-import Lista from './components/Lista';
-import Formularios from './components/Formularios';
-import Saludo from './components/saludo.jsx'
-import Comentario from './components/Comentarios';
+
 
 function App() {
   
-  const data = {
-    nombre: 'Marlon',
-    url: 'http://restauroarte.com/wordpress/wp-content/uploads/2017/10/movil-150x150.png',
-    comentario: 'Comentario del personaje 1'
-  }
-
-  const data2 = {
-    nombre: 'Aurelio',
-    url: 'http://restauroarte.com/wordpress/wp-content/uploads/2017/10/telefono-150x150.png',
-    comentario: 'Comentario del personaje 2'
-  }
 
   return (
     <div className="container mt-5">
       <h1>Hola Marlon</h1>
-      <Contador />
-      <Lista />
-      <Saludo persona='Marlon' />
-      <Saludo persona='Ana' />
-      <Comentario passData={data}/>
-      <Comentario passData={data2}/>
+
     </div>
   );
 }
